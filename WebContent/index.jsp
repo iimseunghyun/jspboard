8b2587e8d0a9e6adda0a7b4f94302d9ac259f0b8
ish <lms-426@hanmail.net>
2020-03-24 11:47:22 +0900

    directory change

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title><%= request.getAttribute("Home")%></title>
 </head>
 <body>
-
+.index page.
 </body>
 </html>
--- src/blog/BlogServelt.java
+++ src/blog/BlogServelt.java
@@ -1,11 +1,14 @@
 package blog;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
 
 
 @WebServlet("/BlogServelt")
@@ -19,8 +22,38 @@ public class BlogServelt extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		if(request.getCharacterEncoding() == null) { //파라미터 인코딩
+			   request.setCharacterEncoding("utf-8");
+			  }
+		String cmd = request.getParameter("cmd");
+		String view = null;
+				
+		if(cmd == null) {
+			view = "index.jsp";
+			cmd = "Home";
+			request.setAttribute(cmd, "Hello!");
+//		}else if(cmd.equals("로그아웃")|| cmd.equals("logout")) {
+//			view = "logout.jsp";
+//			cmd = "logout";
+//			request.setAttribute(cmd, "로그아웃");
+//		}else if(cmd.equals("회원가입")|| cmd.equals("reg")) {       //null.equals("str") : 예외발생
+//				view = "member_reg.jsp";
+//				cmd = "member_reg";
+//				request.setAttribute(cmd, "회원가입 페이지입니다.");
+//		}else if(cmd.equals("정보변경")|| cmd.equals("mod")) {
+//				view = "member_info.jsp";
+//				cmd = "member_mod";
+//				request.setAttribute(cmd, "회원 정보변경 페이지입니다.");
+//		}else if(cmd.equals("로그인")|| cmd.equals("login")) {
+//				view = "member_login.jsp";
+//				cmd = "login"; 
+//				request.setAttribute(cmd, "로그인되었습니다.");
+		}
+//		request.setAttribute(cmd,cmd);
+		RequestDispatcher dis = request.getRequestDispatcher(view);
+		dis.forward(request, response);
 	}
+	
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
